487bc4eba6c9c15007aac8f7420bd26a4fc10c05
Janek7 <janekputz@web.de>
2019-07-24 13:15:55 +0200

    aufgabe 2

--- api-gateway/src/main/java/com/example/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/example/apigateway/ApiGatewayApplication.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableZuulProxy
 public class ApiGatewayApplication {
 
     public static void main(String[] args) {
